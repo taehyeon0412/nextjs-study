e2082c7568eca22f460b2fc6a8ff83723d7bc860
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-11-13 20:01:05 +0900

    fetching data

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,29 +1,50 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-function NavBar() {
+export default function NavBar() {
   const router = useRouter();
-
   return (
     <nav>
-      <Link className={router.pathname === "/" ? "active" : ""} href="/">
-        Home
-      </Link>
+      <img src="/vercel.svg" />
+      <div>
+        <Link href="/" className={router.pathname === "/" ? "active" : ""}>
+          Home
+        </Link>
 
-      <Link
-        className={router.pathname === "/about" ? "active" : ""}
-        href="/about"
-      >
-        About
-      </Link>
+        <Link
+          href="/about"
+          className={router.pathname === "/about" ? "active" : ""}
+        >
+          About
+        </Link>
+      </div>
 
       <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+
         .active {
-          color: green;
+          color: tomato;
+        }
+
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
   );
 }
-
-export default NavBar;
