f3f3d8c8485cb4094d5baeb77ddc910ce0918e6a
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-11-14 18:26:08 +0900

    Redirect and Rewrite
    API키 숨기기
    
    next.config.js를 수정함
    -redirect와 rewrite를 공부
    
    -.env 파일을 만들어 api키를 숨겨줌
    
    -next.config.js에서 rewrite를 이용하여 가짜 url을 넣어줌
    
    -next.config.js에서 받아오는 api는
    process.env.API_KEY로 처리
    
    -.env파일은 깃허브에 올라가지 않도록 .gitignore에 추가해줌

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const API_KEY = process.env.API_KEY;
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/contact/:path*",
+        destination: "/taehyeon202/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
