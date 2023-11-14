8e12b0085c356f106a955cf65b56ff595f357569
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-11-14 22:21:50 +0900

    Movie Detail

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,45 @@
 import { useEffect, useState } from "react";
 import Seo from "./../components/Seo";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title, // url에 보내주는 정보
+        },
+      },
+      `movies/${id}` //사용자한테 보여주는 url 위에서 보내주는 title 정보는 이렇게 숨길수있음
+    );
+  };
+
   return (
     <div className="container">
       <Seo title="Home" />
 
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
-        </div>
+        <Link
+          key={movie.id}
+          href={{
+            pathname: `/movies/${movie.id}`,
+            query: {
+              title: movie.original_title,
+            },
+          }}
+          as={`/movies/${movie.id}`}
+        >
+          <div
+            onClick={() => onClick(movie.id, movie.original_title)}
+            className="movie"
+          >
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+            <h4>{movie.original_title}</h4>
+          </div>
+        </Link>
       ))}
 
       <style jsx>{`
